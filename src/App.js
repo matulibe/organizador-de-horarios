401b6cb549de33d7b9ad5ca0f946e417836662f5
matulibe <57047770+matulibe@users.noreply.github.com>
2023-09-01 01:19:03 -0300

    Se cambia el readme y se agrega descripcion

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import './App.css';
 
+//Las actividades a crear seran widgets. Por lo tanto ademas de implementarlos, la planilla debera ser
+// un mapa de widgets. 
+
+/*
+ * Aqui es donde se renderiza la pagina. Aqui es donde estara renderizada la planilla y sus botones para sus funciones.
+ * Tambien deberan estar renderizadod los widgets de las actividades ya creadas, asi como sus funciones.
+ * 
+ */
 function App() {
   return (
     <div className="App">
